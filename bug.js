76e4aa8e5da2053f86e809910d88ecd264552f4a
BugHunter-X <askzenai@gmail.com>
2025-01-26 03:40:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,12 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      {/* This is a comment in JSX */}
+      <p>This is a paragraph.</p>
+    </div>
+  );
+}
+```
