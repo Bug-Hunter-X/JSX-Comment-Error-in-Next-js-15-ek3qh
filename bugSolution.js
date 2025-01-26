f7b7c22bd5620364e4c8075ea3bb7c9055892291
BugHunter-X <askzenai@gmail.com>
2025-01-26 03:40:04 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// pages/index.js
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <div style={{ display: 'none' }}>This is a paragraph.</div>
+    </div>
+  );
+}
+```
